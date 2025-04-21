88b4d6d148e692f4a873c859ce2bf65c071117c5
oliver130 <olivergtarrow24@gmail.com>
2025-04-21 01:23:35 -0400

    fix auth-url for production2

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import prisma from "@/lib/prisma";
 
 export const authClient = createAuthClient({
   /** the base url of auth server (optional if you're using the same domain) */
-  baseURL: process.env.NEXT_PUBLIC_AUTH_URL,
+  baseURL: process.env.BETTER_AUTH_URL,
 });
 
 export async function signUpWithEmail(formData: FormData) {
